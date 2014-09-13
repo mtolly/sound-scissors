2e5484d6dfe185a721f3992a233a17f70487a5b0
Michael Tolly <miketolly@gmail.com>
2014-09-12 22:33:55 -0500

    Renamed frequency to sample rate

--- src/Sound/Scissors.hs
+++ src/Sound/Scissors.hs
@@ -14,8 +14,8 @@ import Control.Monad
 import Data.Maybe
 import System.Process (callProcess, readProcess)
 
--- | An audio expression, typed by sample frequency and number of channels.
-newtype Audio (freq :: Nat) (chans :: Nat)
+-- | An audio expression, typed by sample rate and number of channels.
+newtype Audio (rate :: Nat) (chans :: Nat)
   = Audio { toRawdio :: Rawdio }
   deriving (Eq, Ord, Show, Read)
 
@@ -44,69 +44,69 @@ unsafeFromRawdio :: Rawdio -> Audio c f
 unsafeFromRawdio = Audio
 
 -- | Glues audio files together sequentially.
-concatenate :: (KnownNat f, KnownNat c) => [Audio f c] -> Audio f c
+concatenate :: (KnownNat r, KnownNat c) => [Audio r c] -> Audio r c
 concatenate []    = silence
 concatenate [aud] = aud
 concatenate auds  = Audio $ Concatenate [ (1, toRawdio aud) | aud <- auds ]
 
 -- | Stacks the channels of two audio files together.
-merge :: Audio f c1 -> Audio f c2 -> Audio f (c1 + c2)
+merge :: Audio r c1 -> Audio r c2 -> Audio r (c1 + c2)
 merge (Audio x) (Audio y) = Audio $ Merge [(1, x), (1, y)]
 
 -- | Mixes audio files together channel-wise.
-mix :: (KnownNat f, KnownNat c) => [Audio f c] -> Audio f c
+mix :: (KnownNat r, KnownNat c) => [Audio r c] -> Audio r c
 mix []    = silence
 mix [aud] = aud
 mix auds  = Audio $ Mix [ (1, toRawdio aud) | aud <- auds ]
 
 -- | An empty audio file, of zero length.
-silence :: (KnownNat f, KnownNat c) => Audio f c
-silence = let res = Audio $ Silence (frequency res) (channels res) in res
+silence :: (KnownNat r, KnownNat c) => Audio r c
+silence = let res = Audio $ Silence (sampleRate res) (channels res) in res
 
 -- | Assigns an already-known sample rate and channel count to an existing file.
-unsafeFile :: FilePath -> Audio f c
+unsafeFile :: FilePath -> Audio r c
 unsafeFile = Audio . File
 
--- | Resamples an audio file from one frequency to another.
+-- | Resamples an audio file from one rate to another.
 resample :: (KnownNat f2) => Audio f1 c -> Audio f2 c
-resample (Audio x) = let res = Audio $ Resample x $ frequency res in res
+resample (Audio x) = let res = Audio $ Resample x $ sampleRate res in res
 
 -- | Pads one end of the audio with silence.
-pad :: Side -> Time -> Audio f c -> Audio f c
+pad :: Side -> Time -> Audio r c -> Audio r c
 pad side len = Audio . Pad    side len . toRawdio
 
 -- | Trims a chunk of time off one end of the audio.
-trim :: Side -> Time -> Audio f c -> Audio f c
+trim :: Side -> Time -> Audio r c -> Audio r c
 trim side len = Audio . Trim   side len . toRawdio
 
 -- | Cuts off everything after a length of time (counted from either end).
-cutoff :: Side -> Time -> Audio f c -> Audio f c
+cutoff :: Side -> Time -> Audio r c -> Audio r c
 cutoff side len = Audio . Cutoff side len . toRawdio
 
 -- | Gets the sample rate stored in the audio file's type.
-frequency :: (KnownNat f) => Audio f c -> Integer
-frequency = natVal . (undefined :: Audio f c -> Proxy f)
+sampleRate :: (KnownNat r) => Audio r c -> Integer
+sampleRate = natVal . (undefined :: Audio r c -> Proxy r)
 
 -- | Gets the number of channels stored in the audio file's type.
-channels :: (KnownNat c) => Audio f c -> Integer
-channels = natVal . (undefined :: Audio f c -> Proxy c)
+channels :: (KnownNat c) => Audio r c -> Integer
+channels = natVal . (undefined :: Audio r c -> Proxy c)
 
 -- | Runs SoX with the given list of arguments. An exception will be raised
 -- if SoX returns a non-zero error code.
 runSox :: [String] -> IO ()
 runSox = callProcess "sox"
 
-getFrequency :: FilePath -> IO Integer
-getFrequency f = fmap read $ readProcess "soxi" ["-r", f] ""
+getRate :: FilePath -> IO Integer
+getRate f = fmap read $ readProcess "soxi" ["-r", f] ""
 
 getChannels :: FilePath -> IO Integer
 getChannels f = fmap read $ readProcess "soxi" ["-c", f] ""
 
-file :: (KnownNat f, KnownNat c) => FilePath -> IO (Maybe (Audio f c))
+file :: (KnownNat r, KnownNat c) => FilePath -> IO (Maybe (Audio r c))
 file fp = do
-  f <- getFrequency fp
+  r <- getRate fp
   c <- getChannels  fp
-  let res = guard (f == frequency aud && c == channels aud)
+  let res = guard (r == sampleRate aud && c == channels aud)
         >> Just (unsafeFile fp)
       aud = fromJust res
   return res
@@ -124,24 +124,24 @@ runRawdio tempdir = go where
     return fp
 
   go x = case x of
-    Silence freq chans -> do
+    Silence rate chans -> do
       output <- new
       runSox
         [ "-n"
         , output
         , "trim", "0", "0"
+        , "rate", show rate
         , "channels", show chans
-        , "rate", show freq
         ]
       return output
     File fp -> return fp
     Concatenate auds -> combine "concatenate" auds
     Merge auds -> combine "merge" auds
     Mix auds -> combine "mix" auds
-    Resample aud freq -> do
+    Resample aud rate -> do
       input <- go aud
       output <- new
-      runSox [input, output, "rate", show freq]
+      runSox [input, output, "rate", show rate]
       return output
     Pad    Front len aud -> transform aud ["pad", showTime len]
     Pad    Back  len aud -> transform aud ["pad", "0", showTime len]
