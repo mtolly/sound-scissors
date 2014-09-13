54f4d3b076c8b710acfb98810bfda8a83e14bd26
Michael Tolly <miketolly@gmail.com>
2014-09-12 21:52:10 -0500

    Renames, docs, cleanup

--- src/Sound/Scissors.hs
+++ src/Sound/Scissors.hs
@@ -13,84 +13,100 @@ import System.IO (hClose)
 import Control.Monad
 import System.Process (callProcess)
 
-data Side = Begin | End
+data Side = Front | Back
   deriving (Eq, Ord, Show, Read, Enum, Bounded)
 
-data Audio'
-  = Silence Integer Integer
-  | File String
-  | Pad Side Double Audio'
-  | Trim Side Double Audio'
-  | Cutoff Side Double Audio'
-  | Concatenate [(Double, Audio')]
-  | Merge [(Double, Audio')]
-  | Mix [(Double, Audio')]
-  | Resample Audio' Integer
+data Time
+  = Seconds Double
+  | Samples Double
   deriving (Eq, Ord, Show, Read)
 
+-- | A raw audio expression.
+data Rawdio
+  = Silence     Integer Integer
+  | File        FilePath
+  | Pad         Side Time Rawdio
+  | Trim        Side Time Rawdio
+  | Cutoff      Side Time Rawdio
+  | Concatenate [(Double, Rawdio)]
+  | Merge       [(Double, Rawdio)]
+  | Mix         [(Double, Rawdio)]
+  | Resample    Rawdio Integer
+  deriving (Eq, Ord, Show, Read)
+
+-- | An audio expression, typed by sample frequency and number of channels.
 newtype Audio (freq :: Nat) (chans :: Nat)
-  = Audio { rawAudio :: Audio' }
+  = Audio { toRawdio :: Rawdio }
   deriving (Eq, Ord, Show, Read)
 
+unsafeFromRawdio :: Rawdio -> Audio c f
+unsafeFromRawdio = Audio
+
+-- | Glues audio files together sequentially.
 concatenate :: (KnownNat f, KnownNat c) => [Audio f c] -> Audio f c
 concatenate []    = silence
 concatenate [aud] = aud
-concatenate auds  = Audio $ Concatenate [ (1, rawAudio aud) | aud <- auds ]
+concatenate auds  = Audio $ Concatenate [ (1, toRawdio aud) | aud <- auds ]
 
+-- | Stacks the channels of two audio files together.
 merge :: Audio f c1 -> Audio f c2 -> Audio f (c1 + c2)
 merge (Audio x) (Audio y) = Audio $ Merge [(1, x), (1, y)]
 
+-- | Mixes audio files together channel-wise.
 mix :: (KnownNat f, KnownNat c) => [Audio f c] -> Audio f c
 mix []    = silence
 mix [aud] = aud
-mix auds  = Audio $ Mix [ (1, rawAudio aud) | aud <- auds ]
+mix auds  = Audio $ Mix [ (1, toRawdio aud) | aud <- auds ]
 
+-- | An empty audio file, of zero length.
 silence :: (KnownNat f, KnownNat c) => Audio f c
-silence = let
-  res = Audio $ Silence (frequency res) (channels res)
-  in res
+silence = let res = Audio $ Silence (frequency res) (channels res) in res
 
-file :: String -> Audio f c
-file = Audio . File
+-- | Assigns an already-known sample rate and channel count to an existing file.
+unsafeFile :: FilePath -> Audio f c
+unsafeFile = Audio . File
 
+-- | Resamples an audio file from one frequency to another.
 resample :: (KnownNat f2) => Audio f1 c -> Audio f2 c
-resample (Audio x) = let
-  res = Audio $ Resample x $ frequency res
-  in res
+resample (Audio x) = let res = Audio $ Resample x $ frequency res in res
 
-pad, trim, cutoff :: Side -> Double -> Audio f c -> Audio f c
-pad    side len = Audio . Pad    side len . rawAudio
-trim   side len = Audio . Trim   side len . rawAudio
-cutoff side len = Audio . Cutoff side len . rawAudio
+-- | Pads one end of the audio with silence.
+pad :: Side -> Time -> Audio f c -> Audio f c
+pad side len = Audio . Pad    side len . toRawdio
 
+-- | Trims a chunk of time off one end of the audio.
+trim :: Side -> Time -> Audio f c -> Audio f c
+trim side len = Audio . Trim   side len . toRawdio
+
+-- | Cuts off everything after a length of time (counted from either end).
+cutoff :: Side -> Time -> Audio f c -> Audio f c
+cutoff side len = Audio . Cutoff side len . toRawdio
+
+-- | Gets the sample rate stored in the audio file's type.
 frequency :: (KnownNat f) => Audio f c -> Integer
-frequency = let
-  makeProxy :: Audio f c -> Proxy f
-  makeProxy _ = Proxy
-  in natVal . makeProxy
+frequency = natVal . (undefined :: Audio f c -> Proxy f)
 
+-- | Gets the number of channels stored in the audio file's type.
 channels :: (KnownNat c) => Audio f c -> Integer
-channels = let
-  makeProxy :: Audio f c -> Proxy c
-  makeProxy _ = Proxy
-  in natVal . makeProxy
+channels = natVal . (undefined :: Audio f c -> Proxy c)
 
-myfile :: Audio 44100 2
-myfile = file "stereo.mp3"
-
-freaky :: Audio 48000 2
-freaky = file "freaky.wav"
-
+-- | Runs SoX with the given list of arguments. An exception will be raised
+-- if SoX returns a non-zero error code.
 runSox :: [String] -> IO ()
 runSox = callProcess "sox"
 
-runAudio :: FilePath -> Audio' -> IO FilePath
-runAudio tempdir = go where
+runRawdio
+  :: FilePath -- ^ A directory for temporary files.
+  -> Rawdio -- ^ Audio expression to evaluate.
+  -> IO FilePath
+runRawdio tempdir = go where
+
   new :: IO FilePath
   new = do
     (fp, h) <- openTempFile tempdir "scissors.wav"
     hClose h
     return fp
+
   go x = case x of
     Silence freq chans -> do
       output <- new
@@ -111,19 +127,25 @@ runAudio tempdir = go where
       output <- new
       runSox [input, output, "rate", show freq]
       return output
-    Pad    Begin len aud -> transform aud ["pad", show len]
-    Pad    End   len aud -> transform aud ["pad", "0", show len]
-    Trim   Begin len aud -> transform aud ["trim", show len]
-    Trim   End   len aud -> transform aud ["trim", "0", show $ negate len]
-    Cutoff Begin len aud -> transform aud ["trim", "0", show len]
-    Cutoff End   len aud -> transform aud ["trim", show $ negate len]
-  transform :: Audio' -> [String] -> IO FilePath
+    Pad    Front len aud -> transform aud ["pad", showTime len]
+    Pad    Back  len aud -> transform aud ["pad", "0", showTime len]
+    Trim   Front len aud -> transform aud ["trim", showTime len]
+    Trim   Back  len aud -> transform aud ["trim", "0", '-' : showTime len]
+    Cutoff Front len aud -> transform aud ["trim", "0", showTime len]
+    Cutoff Back  len aud -> transform aud ["trim", '-' : showTime len]
+
+  showTime :: Time -> String
+  showTime (Seconds d) = show d
+  showTime (Samples d) = show d ++ "s"
+
+  transform :: Rawdio -> [String] -> IO FilePath
   transform aud args = do
     input <- go aud
     output <- new
     runSox $ input : output : args
     return output
-  combine :: String -> [(Double, Audio')] -> IO FilePath
+
+  combine :: String -> [(Double, Rawdio)] -> IO FilePath
   combine method auds = if null $ drop 1 auds
     then error "runAudio: can't combine less than 2 audio files"
     else do
